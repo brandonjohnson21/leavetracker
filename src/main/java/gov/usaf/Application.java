baf1ad04a02b70907009bfd5f091e1dee9ec45ee
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-12-07 09:01:13 -0800

    "runs" in command line

---
+++ src/main/java/gov/usaf/Application.java
@@ -0,0 +1,8 @@
+package gov.usaf;
+
+public class Application {
+    //TODO: how do we want to store data? Database should work, as should files. Ideally databases.
+    public static void main(String[] args) {
+        System.out.println("This application is a library. To use, please add the library to your application and create a new LeaveTracker object for managing leave.");
+    }
+}
